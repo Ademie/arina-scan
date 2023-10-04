b43706f28003868637772725dd899ca9b2ce67d7
ademie <isijolaademie@gmail.com>
2023-10-04 15:06:17 -0600

    Initial Commit

---
+++ arina-scan/arina_scanApp.swift
@@ -0,0 +1,17 @@
+//
+//  arina_scanApp.swift
+//  arina-scan
+//
+//  Created by Ademie  on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct arina_scanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
